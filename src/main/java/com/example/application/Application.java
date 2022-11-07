b070880ea79dfe572810ccd243df18669106122f
ParsaDera-AC <parsa.derakhshan35@gmail.com>
2022-11-07 15:15:35 -0500

    initial repo

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,25 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "hilla-grocery-app")
+@PWA(name = "hilla-grocery-app", shortName = "hilla-grocery-app", offlineResources = {})
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
