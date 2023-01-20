5d977ac558fdddbac0075df738cddd882aec2fa1
Sohrab Nz <naja0021@algonquinlive.com>
2023-01-20 15:15:33 -0500

    Added the Create/Edit UI.

--- frontend/views/events/events-view.ts
+++ frontend/views/events/events-view.ts
@@ -1,47 +1,98 @@
 import '@vaadin/button';
+import '@vaadin/dialog';
+import '@vaadin/text-field';
+import '@vaadin/vertical-layout';
+import '@vaadin/date-picker';
 import '@vaadin/notification';
-import '@vaadin/grid';
+import '@vaadin/vaadin-grid/vaadin-grid';
+import '@vaadin/email-field';
+import '@vaadin/time-picker';
+import '@vaadin/vaadin-grid/vaadin-grid-column';
+import '@vaadin/combo-box';
 import { Notification } from '@vaadin/notification';
 import '@vaadin/text-field';
-import * as HelloWorldEndpoint from 'Frontend/generated/HelloWorldEndpoint';
 import { html } from 'lit';
-import { customElement } from 'lit/decorators.js';
-import { View } from '../view';
-
-import { Person } from '../../../src/main/models/Person';
-//import { getPeople } from '../../../src/main/services/people';
+import { customElement, state } from 'lit/decorators.js';
+import { View } from '../../views/view';
+import { dialogFooterRenderer, dialogRenderer } from '@vaadin/dialog/lit.js';
+import type { DialogOpenedChangedEvent } from '@vaadin/dialog';
 
 @customElement('hello-world-view')
 export class HelloWorldView extends View {
-  private items: Person[] = [];
-
-  async firstUpdated() {
-    
-  }
+  name = '';
 
   connectedCallback() {
     super.connectedCallback();
-    this.items = getDummyPeople();
     this.classList.add('flex', 'p-m', 'gap-m', 'items-end');
   }
-
+  @state()
+  private dialogOpened = true;
+  private items = ['Event type 1', 'Event type 2', 'Event type 3', 'Event type 4'];
+  
   render() {
     return html`
-    <vaadin-grid .items="${this.items}">
-      <vaadin-grid-column path="firstName"></vaadin-grid-column>
-      <vaadin-grid-column path="lastName"></vaadin-grid-column>
-      <vaadin-grid-column path="email"></vaadin-grid-column>
-      <vaadin-grid-column path="profession"></vaadin-grid-column>
-    </vaadin-grid>
+      <div id="user-info">
+      
+        Username: Dave01
+        <br>
+        Name: Dave Chappelle
+      
+      
+        <vaadin-grid id="grid" theme="no-border">
+          <vaadin-grid-column header="Event Name" path="eventName"></vaadin-grid-column>
+          <vaadin-grid-column header="Event Date" path="eventDate"></vaadin-grid-column>
+          <vaadin-grid-column header="Event Confirmation" path="eventConfirm"></vaadin-grid-column>
+        </vaadin-grid>
+      
+        <br>
+        <vaadin-dialog header-title="New Event" .opened="${this.dialogOpened}" @opened-changed="${(e: DialogOpenedChangedEvent) => (this.dialogOpened = e.detail.value)}"
+        ${dialogRenderer(this.renderDialog, [])}
+        ${dialogFooterRenderer(this.renderFooter, [])}
+      ></vaadin-dialog>
+      
+      <vaadin-button @click="${() => (this.dialogOpened = true)}">Create New Event</vaadin-button>
+      <vaadin-button @click="${() => (this.dialogOpened = true)}">Edit Event</vaadin-button>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </div>  
+
+     
+    `;
+  }
+  private renderDialog = () => html`
+    <vaadin-vertical-layout style="align-items: stretch; width: 18rem; max-width: 100%;">
+      <vaadin-text-field label="Event name"></vaadin-text-field>
+      <vaadin-email-field
+      label="Email address"
+      name="email"
+      value=""
+      error-message="Enter a valid email address"
+      clear-button-visible
+      invalid
+      ></vaadin-email-field>
+      <vaadin-date-picker label="Date"></vaadin-date-picker>
+      <vaadin-time-picker label="Time" value="07:00"></vaadin-time-picker>
+      <vaadin-text-field label="Location"></vaadin-text-field>
+      <vaadin-combo-box
+      label="Event type"
+      item-label-path="name"
+      item-value-path="id"
+      .items="${this.items}"
+      ></vaadin-combo-box>
+    </vaadin-vertical-layout>
   `;
+
+  private renderFooter = () => html`
+  <vaadin-button theme="primary" @click="${this.close}">Submit</vaadin-button>
+  <vaadin-button @click="${this.close}">Cancel</vaadin-button>
+  
+`;
+
+private close() {
+    this.dialogOpened = false;
   }
 
-}
+  nameChanged(e: CustomEvent) {
+    this.name = e.detail.value;
+  }
 
-
-function getDummyPeople(): Person[] {
-  return [
-    { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', profession: 'Developer' },
-    { firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com', profession: 'Designer' },
-  ];
 }
